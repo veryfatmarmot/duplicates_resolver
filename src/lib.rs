649ca73b5d596bf01fd073dcfb07a4c4933c47a0
Sergii Bondar <dev.marmot@gmail.com>
2026-01-22 20:06:48 +0000

    print the duplicates result to a json file

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use rayon::prelude::*;
+use serde::Serialize;
 use std::{
     collections::{HashMap, HashSet},
+    fs::File,
+    io::Write,
     path::PathBuf,
 };
 use walkdir::WalkDir;
@@ -12,7 +15,7 @@ type DuplicatesCollection = HashMap<String, Vec<FileDescr>>;
 
 // =============================================================================================
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct FileDescr {
     relative_folder_path: String,
     file_name: String,
@@ -21,39 +24,22 @@ struct FileDescr {
 
 // =============================================================================================
 
-pub fn resolve_duplicates(root_path: &str, threads_count: u8) {
+pub fn resolve_duplicates(root_path: &str, json_path: &str, threads_count: u8) {
     println!("Scanning (with {threads_count} threads) for duplicates in '{root_path}'");
 
     let collection = collect_files(root_path, threads_count);
-    println!("Total files count:{}", collection.len());
+    let total_files_count = collection.values().map(|v| v.len()).sum::<usize>();
 
-    // leave duplicates only
-    let collection: DuplicatesCollection = collection
-        .into_iter()
-        .filter(|(_, paths)| paths.len() > 1)
-        .map(|(size, paths)| {
-            let mut duplicates: Vec<FileDescr> = paths.into_iter()
-            .map(|path| {
-                let file_name = path.file_name().unwrap().to_string_lossy().to_string();
-                let relative_folder_path = path.parent().unwrap().strip_prefix(root_path)
-                    .unwrap()
-                    .to_string_lossy()
-                    .to_string();
-                let relative_folder_path = relative_folder_path.replace('\\', "/");
-                FileDescr {relative_folder_path, file_name, file_size: size}
-            }).collect();
+    let collection: DuplicatesCollection = collect_duplicates(collection, root_path);
+    let duplicates_count = collection.len();
 
-            duplicates.sort_by(|a, b| a.relative_folder_path.cmp(&b.relative_folder_path));
+    save_to_json(collection, json_path);
 
-            (duplicates[0].relative_folder_path.clone(), duplicates)
-        })
-        .collect();    
-
-    println!("Collection:\n{:#?}", collection);
-    println!("Duplicates count:{}", collection.len());
+    println!("Total files count:{total_files_count}");
+    println!("Duplicates count: {duplicates_count} - saved to {json_path}");
 }
 
-fn collect_files(root_path: &str, threads_count: u8) -> FileCollection {    
+fn collect_files(root_path: &str, threads_count: u8) -> FileCollection {
     let pool = rayon::ThreadPoolBuilder::new()
         .num_threads(threads_count as usize)
         .build()
@@ -74,7 +60,7 @@ fn collect_files(root_path: &str, threads_count: u8) -> FileCollection {
             .map(|entry| {
                 let entry = entry.expect("Failed to get WalkDir entry");
                 let metadata = entry.metadata().expect("Failed to get metadata");
-                let file_size = metadata.len();               
+                let file_size = metadata.len();
                 (file_size, entry.into_path())
             })
             .fold_with(FileCollection::new(), |mut acc, (key, value)| {
@@ -90,4 +76,44 @@ fn collect_files(root_path: &str, threads_count: u8) -> FileCollection {
     });
 
     collection
+}
+
+fn collect_duplicates(collection: FileCollection, root_path: &str) -> DuplicatesCollection {
+    collection
+        .into_iter()
+        .filter(|(_, paths)| paths.len() > 1)
+        .map(|(size, paths)| {
+            let mut duplicates: Vec<FileDescr> = paths
+                .into_iter()
+                .map(|path| {
+                    let file_name = path.file_name().unwrap().to_string_lossy().to_string();
+                    let relative_folder_path = path
+                        .parent()
+                        .unwrap()
+                        .strip_prefix(root_path)
+                        .unwrap()
+                        .to_string_lossy()
+                        .to_string();
+                    let relative_folder_path = relative_folder_path.replace('\\', "/");
+                    FileDescr {
+                        relative_folder_path,
+                        file_name,
+                        file_size: size,
+                    }
+                })
+                .collect();
+
+            duplicates.sort_by(|a, b| a.relative_folder_path.cmp(&b.relative_folder_path));
+
+            (duplicates[0].file_name.clone(), duplicates)
+        })
+        .collect()
+}
+
+fn save_to_json(collection: DuplicatesCollection, save_path: &str) {
+    let json = serde_json::to_string_pretty(&collection).expect("Failed to serialize to JSON");
+
+    let mut file = File::create(save_path).expect("Failed to create duplicates.json");
+    file.write_all(json.as_bytes())
+        .expect("Failed to write to duplicates.json");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
-use std::{
-    env,
-};
 use duplicates_resolver::resolve_duplicates;
+use std::env;
 use utils::ScopeTimeLogger;
 
 // =============================================================================================
 
-const USAGE: &str = "Usage: duplicates_resolver <path> --threads=<count>";
+const USAGE: &str = "Usage: duplicates_resolver <path> <json_output_path> --threads=<count>";
 
 // =============================================================================================
 
 fn main() {
     let mut args = env::args();
 
-    let path = args.nth(1).expect(&format!("\nNo path provided.\n{}\n", USAGE));
-    let threads_arg = args.next()
+    let path = args
+        .nth(1)
+        .expect(&format!("\nNo path provided.\n{}\n", USAGE));
+    let json_path = args
+        .next()
+        .expect(&format!("\nNo JSON output path provided.\n{}\n", USAGE));
+    let threads_arg = args
+        .next()
         .expect(&format!("\nNo threads count provided.\n{}\n", USAGE));
-    
+
     let threads_count: u8 = threads_arg
         .trim()
         .strip_prefix("--threads=")
@@ -25,5 +29,5 @@ fn main() {
         .expect("Threads count must be a number");
 
     let _scoped_logger = ScopeTimeLogger::new("Total execution time");
-    resolve_duplicates(&path, threads_count);
+    resolve_duplicates(&path, &json_path, threads_count);
 }
