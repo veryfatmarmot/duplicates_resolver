e5b4be63700034169c037e28c6788ece1823d711
Sergii Bondar <dev.marmot@gmail.com>
2026-01-23 20:05:18 +0000

    Calculate sha2 hash for duplicates. Not used yet

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use rayon::prelude::*;
 use serde::Serialize;
+use sha2::{Digest, Sha256};
 use std::{
     collections::{HashMap, HashSet},
     fs::File,
-    io::Write,
+    io::{Read, Write},
     path::PathBuf,
 };
 use walkdir::WalkDir;
@@ -19,7 +20,7 @@ type DuplicatesCollection = HashMap<String, Vec<FileDescr>>;
 struct FileDescr {
     relative_folder_path: String,
     file_name: String,
-    file_size: u64,
+    hash: String,
 }
 
 // =============================================================================================
@@ -30,7 +31,7 @@ pub fn resolve_duplicates(root_path: &str, json_path: &str, threads_count: u8) {
     let collection = collect_files(root_path, threads_count);
     let total_files_count = collection.values().map(|v| v.len()).sum::<usize>();
 
-    let collection: DuplicatesCollection = collect_duplicates(collection, root_path);
+    let collection: DuplicatesCollection = collect_duplicates(collection, root_path, threads_count);
     let duplicates_count = collection.len();
 
     save_to_json(collection, json_path);
@@ -39,12 +40,22 @@ pub fn resolve_duplicates(root_path: &str, json_path: &str, threads_count: u8) {
     println!("Duplicates count: {duplicates_count} - saved to {json_path}");
 }
 
-fn collect_files(root_path: &str, threads_count: u8) -> FileCollection {
-    let pool = rayon::ThreadPoolBuilder::new()
+fn create_tread_pool(threads_count: u8) -> rayon::ThreadPool {
+    rayon::ThreadPoolBuilder::new()
         .num_threads(threads_count as usize)
         .build()
-        .expect("Failed to build thread pool");
+        .expect("Failed to build thread pool")
+}
 
+fn collect_files(root_path: &str, threads_count: u8) -> FileCollection {
+    let extract_file_info = |entry: Result<walkdir::DirEntry, walkdir::Error>| -> (u64, PathBuf) {
+        let entry = entry.expect("Failed to get WalkDir entry");
+        let metadata = entry.metadata().expect("Failed to get metadata");
+        let file_size = metadata.len();
+        (file_size, entry.into_path())
+    };
+
+    let pool = create_tread_pool(threads_count);
     let collection: FileCollection = pool.install(|| {
         WalkDir::new(root_path)
             .follow_links(false)
@@ -57,12 +68,7 @@ fn collect_files(root_path: &str, threads_count: u8) -> FileCollection {
                 }
             })
             .par_bridge()
-            .map(|entry| {
-                let entry = entry.expect("Failed to get WalkDir entry");
-                let metadata = entry.metadata().expect("Failed to get metadata");
-                let file_size = metadata.len();
-                (file_size, entry.into_path())
-            })
+            .map(extract_file_info)
             .fold_with(FileCollection::new(), |mut acc, (key, value)| {
                 acc.entry(key).or_insert_with(HashSet::new).insert(value);
                 acc
@@ -78,42 +84,79 @@ fn collect_files(root_path: &str, threads_count: u8) -> FileCollection {
     collection
 }
 
-fn collect_duplicates(collection: FileCollection, root_path: &str) -> DuplicatesCollection {
+fn collect_duplicates(
+    collection: FileCollection,
+    root_path: &str,
+    threads_count: u8,
+) -> DuplicatesCollection {
+    let pool = create_tread_pool(threads_count);
+    let collection: DuplicatesCollection = pool.install(|| {
+        collection
+            .into_iter()
+            .filter(|(_, paths)| paths.len() > 1)
+            .par_bridge()
+            .map(|(_, paths)| extract_duplicate_descr(paths, root_path))
+            .collect()
+    });
+
     collection
+}
+
+fn extract_duplicate_descr(paths: HashSet<PathBuf>, root_path: &str) -> (String, Vec<FileDescr>) {
+    let mut duplicates: Vec<FileDescr> = paths
         .into_iter()
-        .filter(|(_, paths)| paths.len() > 1)
-        .map(|(size, paths)| {
-            let mut duplicates: Vec<FileDescr> = paths
-                .into_iter()
-                .map(|path| {
-                    let file_name = path.file_name().unwrap().to_string_lossy().to_string();
-                    let relative_folder_path = path
-                        .parent()
-                        .unwrap()
-                        .strip_prefix(root_path)
-                        .unwrap()
-                        .to_string_lossy()
-                        .to_string();
-                    let relative_folder_path = relative_folder_path.replace('\\', "/");
-                    FileDescr {
-                        relative_folder_path,
-                        file_name,
-                        file_size: size,
-                    }
-                })
-                .collect();
-
-            duplicates.sort_by(|a, b| a.relative_folder_path.cmp(&b.relative_folder_path));
-
-            (duplicates[0].file_name.clone(), duplicates)
+        .map(|path| {
+            let file_name = path.file_name().unwrap().to_string_lossy().to_string();
+            let relative_folder_path = path
+                .parent()
+                .unwrap()
+                .strip_prefix(root_path)
+                .unwrap()
+                .to_string_lossy()
+                .to_string();
+            let relative_folder_path = relative_folder_path.replace('\\', "/");
+            let hash = calculate_hash(&path);
+            FileDescr {
+                relative_folder_path,
+                file_name,
+                hash,
+            }
         })
-        .collect()
+        .collect();
+
+    duplicates.sort_by(|a, b| a.relative_folder_path.cmp(&b.relative_folder_path));
+
+    (duplicates[0].file_name.clone(), duplicates)
 }
 
 fn save_to_json(collection: DuplicatesCollection, save_path: &str) {
     let json = serde_json::to_string_pretty(&collection).expect("Failed to serialize to JSON");
 
-    let mut file = File::create(save_path).expect("Failed to create duplicates.json");
+    let path = PathBuf::from(save_path);
+    if let Some(parent) = path.parent() {
+        std::fs::create_dir_all(parent).expect("Failed to create directories");
+    }
+
+    let mut file = File::create(save_path).expect("Failed to create {save_path}");
     file.write_all(json.as_bytes())
-        .expect("Failed to write to duplicates.json");
+        .expect("Failed to write to {save_path}");
+}
+
+fn calculate_hash(path: &PathBuf) -> String {
+    let mut file = File::open(path).expect("Failed to open file for hashing");
+    let mut hasher = Sha256::new();
+    let mut buffer = [0u8; 8192];
+
+    loop {
+        let bytes_read = file
+            .read(&mut buffer)
+            .expect("Failed to read file for hashing");
+        if bytes_read == 0 {
+            break;
+        }
+        hasher.update(&buffer[..bytes_read]);
+    }
+
+    let hash_result = hasher.finalize();
+    format!("{:x}", hash_result)
 }
